26f5e3b41ed24f006735ffba43b5bfd3ec869be7
Shashwat2121 <93264561+Shashwat2121@users.noreply.github.com>
2021-10-28 14:07:39 +0530

    Delete output.tf

--- LAB4/output.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "outputs the ID of VPC"
-}
-
-output "ec2_public_ip" {
-  value       = aws_instance.main.public_ip
-  description = "outputs the public IP of ec2 instance"
-}
