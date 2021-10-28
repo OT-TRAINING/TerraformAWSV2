d72c996b6e04f54d58dd3eb3db0b7c7baaa6460d
rishabhhsharmaa <93264358+rishabhhsharmaa@users.noreply.github.com>
2021-10-28 18:13:28 +0530

    Update main.tf

--- Lab5/main.tf
+++ Lab5/main.tf
@@ -1,33 +1,111 @@
 provider "aws" {
-  region = "us-east"
+  region = "us-east-1"
 }
 
 #  create VPC
 resource "aws_vpc" "main" {
-  cidr_block       =    var.cidr_block_vpc
-  instance_tenancy = var.instance_tenancy_vpc
+  cidr_block       = var.cidr_block_vpc
+  instance_tenancy = "default"
   tags = {
-    Name = var.Name_vpc
+    Name = "test-vpc"
   }
 }
 
-# create public subnet
-resource "aws_subnet" "main" {
+resource "aws_subnet" "pub_subnet1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              =   var.cidr_subnet
-  map_public_ip_on_launch = var.public_ip_on_launch
+  cidr_block              = var.cidr_pub_subnet_1
+  map_public_ip_on_launch = true
   tags = {
-    Name = var.name_subnet
+    Name = "Public-subnet-1"
   }
 }
 
-# create ec-2
+resource "aws_subnet" "pub_subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_pub_subnet_2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "pvt_subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_private_subnet_1
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "pvt_subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_private_subnet_2
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Private-subnet-2"
+  }
+}
+
+# Creating EC2 Instance
 resource "aws_instance" "main" {
-  ami           = var.ami_ec2
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  ami                         = "ami-082d645d7cbf18b97"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.main.id]
+  subnet_id                   = aws_subnet.pub_subnet1.id
   tags = {
-    Name = var.Name_ec2
+    Name = "test-ec2-instance"
   }
 }
 
+# Creating IGW 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "test-igw"
+  }
+}
+
+# Creating SG
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+  name   = "main Security Group"
+
+  # allow ingress of port 80
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  # allow egress of all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "test-SG"
+  }
+}
+
+# Creating Route table
+resource "aws_route_table" "igw_route" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "test-route-table"
+  }
+}
+
+# Route table association
+resource "aws_main_route_table_association" "rtb_association" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.igw_route.id
+}
